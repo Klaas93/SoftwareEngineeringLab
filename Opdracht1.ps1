c9deacc95b11f77ac5aa2d0c728324a8a9fab3bf
klaas <klaas-descamps@hotmail.com>
2022-02-18 17:05:12 +0100

    Remove the answers for the Word question in comments + add comments in script

--- Opdracht1.ps1
+++ Opdracht1.ps1
@@ -1,26 +1,5 @@
 ﻿# Automatiseren software-installatie
 
-# Vragen
-#
-# De PowerShell-prompt toont de map waar we ons nu bevinden. Wat is de naam van deze directory?
-# ==> C:\WINDOWS\System32
-#
-# In welke map heb je het script bewaard?
-# ==> C:\Users\klaas\Documents\hogeschool\SEL\Opdracht1
-#
-# In welke map is het script bewaard in screenshot in Figuur 4?
-# ==> D:\Users\BertVV\Documents\HoGent\SELab\
-#
-# Commands:
-# Lijst tonen van software geïnstalleerd door chocolatey
-# ==> choco list --local-only
-# Alle geïnstalleerd packages bijwerken naar laatste versie
-# ==> choco upgrade all
-# Via console package opzoeken
-# ==> choco search {package}
-# Een geïnstalleerd programma verwijderen
-# ==> choco uninstall {package}
-
 #  ====   Write script to complete challenges ====
 
 # Wait until user is ready
@@ -33,12 +12,13 @@ Pause;
 # Change directory naar map waar script wordt bewaard
 $directory = Get-Location;
 Write-Host "Current directory" $directory -ForegroundColor Yellow;
-Set-Location C:\Users\klaas\Documents\hogeschool\SEL\Opdracht1;
+Set-Location C:\Users\klaas\Documents\HoGent\SEL\Opdracht1;
 $directory = Get-Location;
 Write-Host "New set directory" $directory -ForegroundColor Yellow;
 # Wait until user is ready
 Write-Host "Press a key to move to the next step: Run the script to install software" -ForegroundColor DarkCyan;
 PAUSE;
+# Start installation script
 Write-Host "Start script InstallSoftware.ps1" -ForegroundColor Yellow;
 .\InstallSoftware.ps1;
 Write-Host "End script Installatie.ps1" -ForegroundColor Green;
